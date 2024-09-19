9a307d7c4e4d7d116c56cc3d0e5a74391e220305
StjepanMaric <stjepan.maric1994@gmail.com>
2024-09-19 23:02:32 +0200

    Day24 code

---
+++ day24/main.py
@@ -0,0 +1,17 @@
+with open("input/letters/starting_letter.txt","r") as letter:
+   letter_content = letter.readlines()
+
+with open("input/names/invited_names.txt","r") as names:
+   guests = names.read().splitlines()
+
+for guest in guests:
+    with open(f"output/readyToSend/inviteFor{guest}.txt", "w") as output:
+        output.write(f"Dear {guest},\n" + "".join(letter_content[1:]))
+
+
+for guest in guests:
+    with open(f"output/readyToSend/inviteFor{guest}.txt", "w") as output:
+        pre_name = letter_content[0].split(" ")[0]
+        name = letter_content[0].split(" ")[1].replace("[name]", guest)
+        line = pre_name + " " + name
+        output.write(line + "".join(letter_content[1:]))

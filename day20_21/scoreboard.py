60f66d544a611740a897293ca15a8ea3d78b94e2
StjepanMaric <stjepan.maric1994@gmail.com>
2024-09-19 22:29:14 +0200

    DAY 24 Added file system for tracking of highscore to snake game

--- day20_21/scoreboard.py
+++ day20_21/scoreboard.py
@@ -1,12 +1,14 @@
 from turtle import Turtle
 ALIGNMENT = "center"
 FONT = ("Courier", 15, "normal")
-
+fin = open("scores.txt", "r")
+HIGHSCORE = fin.read()
 
 class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.highscore = int(HIGHSCORE)
         self.color("white")
         self.penup()
         self.hideturtle()
@@ -15,13 +17,23 @@ class Scoreboard(Turtle):
         self.update_scoreboard()
 
     def update_scoreboard(self):
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.clear()
+        self.write(f"Score: {self.score} High Score:{self.highscore}" , align=ALIGNMENT, font=FONT)
 
-    def game_over(self):
+    def reset(self):
+        if self.score > self.highscore:
+            self.highscore = self.score
+        self.score = 0
+        self.update_scoreboard()
+        fout = open("scores.txt", "w")
+        fout.write(str(self.highscore))
+        fout.close()
+    """def game_over(self):
         self.goto(0,0)
-        self.write("GAME OVER", align= ALIGNMENT, font=FONT)
+        self.write("GAME OVER", align= ALIGNMENT, font=FONT)"""
 
     def increase_score(self):
         self.score += 1
         self.clear()
         self.update_scoreboard()
+

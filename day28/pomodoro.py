2a40d814f9313ccaa88da633812de3a17d9e7fb9
StjepanMaric <stjepan.maric1994@gmail.com>
2024-09-24 22:39:06 +0200

    Day 28 Pomodoro Timer App part 1 - UI

---
+++ day28/pomodoro.py
@@ -0,0 +1,44 @@
+from tkinter import *
+import time
+# ---------------------------- CONSTANTS ------------------------------- #
+PINK = "#e2979c"
+RED = "#e7305b"
+GREEN = "#9bdeac"
+YELLOW = "#f7f5dd"
+CHECKMARK = "✔"
+FONT_NAME = "Courier"
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
+
+# ---------------------------- TIMER RESET ------------------------------- #
+
+# ---------------------------- TIMER MECHANISM ------------------------------- #
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+
+# ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Pomodoro technique timer")
+window.config(padx=100, pady=50, bg=YELLOW)
+
+title_label = Label(text="Timer",font=(FONT_NAME,35,"bold"), fg=GREEN, bg=YELLOW)
+title_label.grid(row=0, column=1)
+
+canvas = Canvas(window, width=200,height=224, bg=YELLOW, highlightthickness=0)
+tomato_image = PhotoImage(file="tomato.png")
+canvas.create_image(100, 112, image=tomato_image)
+canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME,35,"bold"))
+canvas.grid(row=1, column=1)
+
+start_button=Button(text="Start", highlightthickness=0)
+start_button.grid(row=2, column=0)
+
+reset_button=Button(text="Reset", highlightthickness=0)
+reset_button.grid(row=2, column=2)
+
+label_checkmark = Label(text=CHECKMARK, font=(FONT_NAME,15,"bold"), fg=GREEN, bg=YELLOW)
+label_checkmark.grid(row=3, column=1)
+
+
+window.mainloop()

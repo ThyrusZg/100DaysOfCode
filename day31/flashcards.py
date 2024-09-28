9c52c6cc445ef1e4e947097237cfa82d6b0549db
StjepanMaric <stjepan.maric1994@gmail.com>
2024-09-28 21:48:33 +0200

    Day 31 start Flashcards

---
+++ day31/flashcards.py
@@ -0,0 +1,13 @@
+from tkinter import *
+
+BACKGROUND_COLOR = "#B1DDC6"
+CHECKMARK = "✔"
+INCORRECT = "X"
+FONT_NAME = "Courier"
+
+
+window = Tk()
+window.title("Flashcards")
+window.config(padx=100, pady=50, bg=BACKGROUND_COLOR)
+
+window.mainloop()
